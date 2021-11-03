5c20b34fa8b74566e4cde65c2d2486ceb8edd379
thtrangg <nguyenthutrang01bn@gmail.com>
2021-11-03 10:57:17 +0700

    login & register

--- js/login.js
+++ js/login.js
@@ -1,8 +1,63 @@
+// for modal
 var modal = document.getElementById("id01");
-
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 };
+
+$().ready(function () {
+  // đăng nhập
+  $("#login-modal-btn_login").on("click", function () {
+    handleLoginClick();
+  });
+
+  // đăng ký
+  $("#login-modal-btn_register").on("click", function () {
+    handleRegisterClick();
+  });
+});
+
+function handleLoginClick() {
+  // lấy dữ liệu từ form đăng nhập
+  let username = $("#login_username").val();
+  let password = $("#login_password").val();
+
+  // call API
+  let url = API_URI + "/login";
+  let authorization = "Basic " + btoa(username + ":" + password);
+  $.ajax({
+    url: url,
+    type: "GET",
+    beforeSend: function (xhr) {
+      xhr.setRequestHeader("Authorization", authorization);
+    },
+    success: function (data, textStatus, xhr) {
+      alert(data);
+    },
+    error(jqXHR, textStatus, errorThrown) {
+      alert("Sai tên đăng nhập hoặc mật khẩu");
+    },
+  });
+}
+
+function handleRegisterClick() {
+  // lấy dữ liệu từ form đăng ký
+  let fullname = $("register_fullname").val();
+  let email = $("register_email").val();
+  let phoneNumber = $("register_phoneNumber").val();
+  let address = $("register_address").val();
+  let username = $("register_username").val();
+  let password = $("register_password").val();
+
+  let registerForm = {
+    fullname: fullname,
+    email: email,
+    phoneNumber: phoneNumber,
+    address: address,
+    username: username,
+    password: password,
+  };
+  console.log(registerForm);
+}
--- js/register.js
+++ js/register.js
@@ -1,9 +1,9 @@
 // Get the modal
-var modal = document.getElementById("id02");
+var modal_register = document.getElementById("id02");
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-  if (event.target == modal) {
+  if (event.target == modal_register) {
     modal.style.display = "none";
   }
 };
