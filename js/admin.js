2495b2448b61c56e7c8203681f9a51d084422858
thtrangg <nguyenthutrang01bn@gmail.com>
2021-11-11 21:26:14 +0700

    create btn update

--- js/admin.js
+++ js/admin.js
@@ -5,6 +5,7 @@ var listBook = [];
 var listAuthor = [];
 var listPublisher = [];
 var listCategory = [];
+var update_id = 0;
 
 // on ready
 $(document).ready(function () {
@@ -151,11 +152,13 @@ function sendCreateBook() {
     },
     success: function (data, textStatus, xhr) {
       // close create modal
-      $(".cancel").click();
+      // $(".cancel").click();
+      update_id = 0;
       getBooksFromAPI();
     },
     error(jqXHR, textStatus, errorThrown) {
       // alert("Lỗi xác thực");
+      update_id = 0;
       getAuthorFromAPI();
     },
   });
@@ -185,6 +188,131 @@ function sendDeleteBook(id) {
       }
     },
   });
+}
+
+function sendUpdateBook() {
+  let id = update_id;
+  if (id <= 0) {
+    return;
+  }
+  // get data from form modal
+  let book_name = $("#book-name").val();
+  let book_img = $("#book-img").val();
+  let book_oldPrice = $("#book-oldPrice").val();
+  let book_newPrice = $("#book-newPrice").val();
+  let book_year = $("#book-year").val();
+  let book_author = $("#book-select-author").val();
+  let book_category = $("#book-select-category").val();
+  let book_publisher = $("#book-select-publisher").val();
+
+  //
+  let book = {
+    img: book_img,
+    name: book_name,
+    oldPrice: book_oldPrice,
+    newPrice: book_newPrice,
+    year: book_year,
+    authorId: book_author,
+    categoryId: book_category,
+    publisherId: book_publisher,
+  };
+
+  // get token
+  let authorization = localStorage.getItem("authorization");
+
+  // call API
+  let url = API_URI + "/book/update/" + id;
+  $.ajax({
+    url: url,
+    type: "PUT",
+    data: JSON.stringify(book),
+    contentType: "application/json",
+    beforeSend: function (xhr) {
+      xhr.setRequestHeader("Authorization", authorization);
+    },
+    success: function (data, textStatus, xhr) {
+      // close create modal
+      update_id = 0;
+      // $(".cancel").click();
+      getBooksFromAPI();
+    },
+    error(jqXHR, textStatus, errorThrown) {
+      // alert("Lỗi xác thực");
+      update_id = 0;
+      getAuthorFromAPI();
+    },
+  });
+}
+
+//
+function editOnClick(id) {
+  update_id = id;
+  // get book update
+  let updateBook = null;
+  for (const book of listBook) {
+    if (book.id == id) {
+      updateBook = book;
+      break;
+    }
+  }
+  // get category, author, publisher id
+  for (const author of listAuthor) {
+    if (author.name === updateBook.author) {
+      updateBook.authorId = author.id;
+    }
+  }
+  for (const category of listCategory) {
+    if (category.name === updateBook.category) {
+      updateBook.categoryId = category.id;
+    }
+  }
+  for (const publisher of listPublisher) {
+    if (publisher.name === updateBook.publisher) {
+      updateBook.publisherId = publisher.id;
+    }
+  }
+
+  // check
+  if (updateBook == null) {
+    alert("đã xảy ra lỗi");
+  }
+  // fill data to modal
+  $("#book-name").val(updateBook.name);
+  $("#book-img").val(updateBook.img);
+  $("#book-oldPrice").val(updateBook.oldPrice);
+  $("#book-newPrice").val(updateBook.newPrice);
+  $("#book-year").val(updateBook.year);
+  $("#book-select-author")
+    .find("option")
+    .each(function () {
+      $(this).attr("selected", false);
+
+      if ($(this).val() == updateBook.authorId) {
+        $(this).attr("selected", true);
+      }
+    });
+
+  $("#book-select-category")
+    .find("option")
+    .each(function () {
+      $(this).attr("selected", false);
+
+      if ($(this).val() == updateBook.categoryId) {
+        $(this).attr("selected", true);
+      }
+    });
+
+  $("#book-select-publisher")
+    .find("option")
+    .each(function () {
+      $(this).attr("selected", false);
+
+      if ($(this).val() == updateBook.publisherId) {
+        $(this).attr("selected", true);
+      }
+    });
+  // show modal
+  document.getElementById("btn-add-book").click();
 }
 
 /// display data
@@ -230,7 +358,7 @@ function book2row(book) {
   <td>${book.newPrice}</td>
   <td>${book.year}</td>
   <td><button class="btn btn-danger" onclick="deleteById(${book.id})">Xóa</button></td>
-  <td><button class="btn btn-success" onclick="updateById(${book.id})">Sửa</button></td>
+  <td><button class="btn btn-success" onclick="editOnClick(${book.id})">Sửa</button></td>
 </tr>`;
 }
 
